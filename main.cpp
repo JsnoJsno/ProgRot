cc3099e0a35e39625fb86058ffaa25fb249df5ad
JsnoJsno <jsnojsno666@gmail.com>
2025-08-06 01:25:16 +0300

    bug fix 1

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 int main(int argc, char**argv) {
 
     /*  read file  */
-    std::ifstream code_file;
+    std::ifstream code_file(argv[1]);
     std::string code;
     std::string buffer;
 
-    while (!std::getline(code_file, buffer)) {
-        code.append(buffer);
+    while (std::getline(code_file, buffer)) {
+        code += buffer + "\n";
     }
     
     std::cout << code;
+    return 0;
 }
